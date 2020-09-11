1fcf8ebb010414dee62bccf8e0b042feef8f3015
Ed C <ecabu001@ucr.edu>
2020-09-10 19:44:52 -0700

    added comment to views.py

--- weather_looker/urls.py
+++ weather_looker/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 #insert new URLS here
+#This is a new comment
 urlpatterns = [
     path('', views.home, name="home"),
     path('about.html', views.about, name="about")
